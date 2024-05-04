88dc2820982e47ccdbe4bdcb3c9662f24204bf57
karthikeyanselvam18 <karthikeyanselvam18@gmail.com>
2024-05-05 00:52:52 +0530

    added more style

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+const features = document.querySelectorAll(".card");
+
+features.forEach((feature, index) => {
+  let animation;
+  switch (index % 2) {
+    case 0:
+      animation = "fade-left";
+      break;
+    case 1:
+      animation = "fade-right";
+      break;
+  }
+  feature.setAttribute("data-aos", animation);
+});
+
+const testimonials = document.querySelectorAll(".testimonial");
+testimonials.forEach((testimonial) => {
+  let animation = "zoom-in";
+  testimonial.setAttribute("data-aos", animation);
+});
+AOS.init();
+
+const burger = document.querySelector("#burger");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("change", () => {
+  const isNavOpened = burger.checked;
+  nav.style.transform = isNavOpened ? "scaleY(1)" : "scaleY(0)";
+});
+
+window.addEventListener('resize', ()=>{
+  if(window.innerWidth>768){
+    nav.style.transform = "scaleY(1)";
+    burger.checked = false
+  } else{
+    nav.style.transform = "scaleY(0)";
+    burger.checked = false
+  }
+})
